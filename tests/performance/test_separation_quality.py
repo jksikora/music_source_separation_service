056ad04fd9bd3dc4e5dcd59230b814841883063c
Jakub Sikora <jakubsikora515@gmail.com>
2025-11-29 13:41:57 +0100

    test: added working test_separation_quality but NaNs need to be fixed

--- tests/performance/test_separation_quality.py
+++ tests/performance/test_separation_quality.py
@@ -1,6 +1,7 @@
 import soundfile as sf
 import numpy as np
-import musdb, museval, io, csv, os, yaml, httpx
+import musdb, museval, io, csv, os, yaml, httpx, json
+import matplotlib.pyplot as plt
 
 # --- Separation Quality Benchmark Test ---
 
@@ -26,7 +27,7 @@ csv_file = os.path.join(results_dir, "separation_quality_results.csv") # Complet
 if not os.path.exists(csv_file):
     with open(csv_file, "w", newline="") as f:
         writer = csv.writer(f)
-        writer.writerow(["filename", "stem", "SDR"])
+        writer.writerow(["Filename", "Stem", "Median SDR", "Median SIR", "Median SAR", "Median ISR"])
 
 
 # === Convert Audio to Bytes Helper Function ===
@@ -39,65 +40,54 @@ def audio_to_bytes(audio, sample_rate):
 
 # === Test Separation Quality Function ===
 def test_separation_quality():
-    for track in mus.tracks[:1]:  # Limit to first 1 for testing; remove [:1] for full test
+    for track in mus.tracks[:2]:  # Limit to first 1 for testing; remove [:1] for full test
         print(f"\nstart processing file: {track.name}")
         
-        input_waveform = track.audio # Get mixture audio from musdb track
-        input_sample_rate = track.rate
-        input_audio_bytes = audio_to_bytes(input_waveform, input_sample_rate) # Convert mixture from numpy.ndarray to bytes
-        
+        input_audio_bytes = audio_to_bytes(track.audio, track.rate) # Convert musdb mixture from numpy.ndarray to bytes
         files = {'file': (f'{track.name}.wav', input_audio_bytes, 'audio/wav')}
         upload_response = client.post("/upload_audio", files=files)
+
         if upload_response.status_code != 200:
             print(f"unexpected status code from upload_audio endpoint response: {upload_response.status_code} for file: {track.name}")
             continue # Skip to next track on failure
         
         data = upload_response.json()
-        print("SERVER RETURN ORDER:", list(data.keys()))
-
         estimates = {}
-        references = {}
-        stem_order = list(data.keys())  # Use the order returned by the server
-        for stem_name in stem_order:
-            file_id = data[stem_name]['file_id']
+        for stem_name, stem_data in data.items():
+            file_id = stem_data['file_id']
             download_resp = client.get(f"/download_audio/{file_id}")
             if download_resp.status_code == 200:
                 buffer = io.BytesIO(download_resp.content)
                 output_waveform, _ = sf.read(buffer)
                 estimates[stem_name] = output_waveform # Transpose because museval expects (channels, samples)
                 print(f"estimated stem {stem_name} of file: {track.name} shape: {estimates[stem_name].shape}")
+                print(f"reference stem {stem_name} of file: {track.name} shape: {track.targets[stem_name].audio.shape}")
+
             else:
                 print(f"unexpected status code from download_audio endpoint response: {download_resp.status_code} for {stem_name} of file: {track.name}")
             
-            references[stem_name] = track.targets[stem_name].audio  # Transpose because museval expects (channels, samples)
-            print(f"reference stem {stem_name} of file: {track.name} shape: {track.targets[stem_name].audio.shape}")
-        
         try:
-            ref_arr = np.stack([references[s] for s in stem_order])
-            est_arr = np.stack([estimates[s] for s in stem_order])
-            print(f"ref_arr shape: {ref_arr.shape}, est_arr shape: {est_arr.shape}")
+            museval.eval_mus_track(track, estimates, output_dir=results_dir)
 
-            print("museval.evaluate =", museval.evaluate)
-
-            results = museval.evaluate(ref_arr, est_arr) # Results is a list of DataFrames, one DataFrame per stem; Default frame length and hop equals 1 second
-            print(type(results[0]))
-            with open(csv_file, 'a', newline='') as f:
-                writer = csv.writer(f)
-                for stem_name, dataframe in zip(stem_order, results):
-                    sdr = dataframe["SDR"].median()  # According to The 2018 Signal Separation Evaluation Campaign paper, median is preferred over mean for SDR
-                    writer.writerow([track.name, stem_name, sdr])
-                    assert sdr is not None, f"SDR is None for stem {stem_name} of file: {track.name}"
-                    print(f" SDR = {sdr:.2f} for stem {stem_name} of file: {track.name}")
-                # for stem_name in stem_order:
-                #     print(stem_name, "silent:", np.allclose(references[stem_name], 0))
-                #     sdr_median = results[stem_order.index(stem_name)]['SDR'].median()  # According to The 2018 Signal Separation Evaluation Campaign paper, median is preferred over mean for SDR
-                #     print(f"reference stem {stem_name} shape:{references[stem_name].shape}", f"estimate stem {stem_name} shape:{estimates[stem_name].shape}")
-                #     writer.writerow([track.name, stem_name, sdr_median])
-                #     assert sdr_median is not None, f"SDR is None for stem {stem_name} of file: {track.name}"
-                #     print(f" SDR = {sdr_median:.2f} for stem {stem_name} of file: {track.name}")
+            test_output = os.path.join(results_dir, "test", f"{track.name}.json")
+            metrics = ["SDR", "SIR", "SAR", "ISR"]
+            with open(test_output, 'r') as jf:
+                data = json.load(jf)
+                
+            with open(csv_file, 'a', newline='') as cf:
+                writer = csv.writer(cf)
+                for target in data.get("targets"):
+                    stem_name = target.get("name")
+                    frames = target.get("frames")
+                    metrics_medians = []
+                    for metric in metrics:
+                        values = [frame["metrics"][metric] for frame in frames if np.isfinite(frame["metrics"][metric])] # Filter out NaN values
+                        metrics_medians.append(np.median(values))
+                    writer.writerow([track.name, stem_name] + metrics_medians)
+                    print(f"Medians for {stem_name}: SDR={metrics_medians[0]:.2f}, SIR={metrics_medians[1]:.2f}, SAR={metrics_medians[2]:.2f}, ISR={metrics_medians[3]:.2f}")
 
         except Exception as e:
-            assert False, f"evaluation failed for: {track.name}; {e}"
+            print(f"evaluation failed for: {track.name} with error: {e}")
 
 
 if __name__ == "__main__":
