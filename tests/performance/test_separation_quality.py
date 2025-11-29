bf3b43cf1e8fb4dce0e7d3669a11c199aa646495
Jakub Sikora <jakubsikora515@gmail.com>
2025-11-29 23:21:49 +0100

    test: fully functional version of test_separation_quality

--- tests/performance/test_separation_quality.py
+++ tests/performance/test_separation_quality.py
@@ -1,7 +1,6 @@
 import soundfile as sf
 import numpy as np
 import musdb, museval, io, csv, os, yaml, httpx, json
-import matplotlib.pyplot as plt
 
 # --- Separation Quality Benchmark Test ---
 
@@ -40,7 +39,8 @@ def audio_to_bytes(audio, sample_rate):
 
 # === Test Separation Quality Function ===
 def test_separation_quality():
-    for track in mus.tracks[:2]:  # Limit to first 1 for testing; remove [:1] for full test
+    results = museval.EvalStore() # Initialize EvalStore to hold separation quality results
+    for track in mus.tracks:  # Limit to first 1 for testing; remove [:1] for full test
         print(f"\nstart processing file: {track.name}")
         
         input_audio_bytes = audio_to_bytes(track.audio, track.rate) # Convert musdb mixture from numpy.ndarray to bytes
@@ -51,7 +51,7 @@ def test_separation_quality():
             print(f"unexpected status code from upload_audio endpoint response: {upload_response.status_code} for file: {track.name}")
             continue # Skip to next track on failure
         
-        data = upload_response.json()
+        data = upload_response.json() # Get separation result metadata
         estimates = {}
         for stem_name, stem_data in data.items():
             file_id = stem_data['file_id']
@@ -59,7 +59,7 @@ def test_separation_quality():
             if download_resp.status_code == 200:
                 buffer = io.BytesIO(download_resp.content)
                 output_waveform, _ = sf.read(buffer)
-                estimates[stem_name] = output_waveform # Transpose because museval expects (channels, samples)
+                estimates[stem_name] = output_waveform # Store estimated stem waveform
                 print(f"estimated stem {stem_name} of file: {track.name} shape: {estimates[stem_name].shape}")
                 print(f"reference stem {stem_name} of file: {track.name} shape: {track.targets[stem_name].audio.shape}")
 
@@ -67,28 +67,37 @@ def test_separation_quality():
                 print(f"unexpected status code from download_audio endpoint response: {download_resp.status_code} for {stem_name} of file: {track.name}")
             
         try:
-            museval.eval_mus_track(track, estimates, output_dir=results_dir)
-
-            test_output = os.path.join(results_dir, "test", f"{track.name}.json")
-            metrics = ["SDR", "SIR", "SAR", "ISR"]
-            with open(test_output, 'r') as jf:
-                data = json.load(jf)
-                
-            with open(csv_file, 'a', newline='') as cf:
-                writer = csv.writer(cf)
-                for target in data.get("targets"):
-                    stem_name = target.get("name")
-                    frames = target.get("frames")
-                    metrics_medians = []
-                    for metric in metrics:
-                        values = [frame["metrics"][metric] for frame in frames if np.isfinite(frame["metrics"][metric])] # Filter out NaN values
-                        metrics_medians.append(np.median(values))
-                    writer.writerow([track.name, stem_name] + metrics_medians)
-                    print(f"Medians for {stem_name}: SDR={metrics_medians[0]:.2f}, SIR={metrics_medians[1]:.2f}, SAR={metrics_medians[2]:.2f}, ISR={metrics_medians[3]:.2f}")
+            scores = museval.eval_mus_track(track, estimates, output_dir=results_dir) # Evaluate separation quality track by track and save results to json files
+            results.add_track(scores) # Add scores to EvalStore to have overall results in pandas dataframe and to aggregate metrics (median, mean / by frame, stem, tracks)
 
         except Exception as e:
             print(f"evaluation failed for: {track.name} with error: {e}")
+    
+    results.save(os.path.join(results_dir, "separation_quality_results_eval.pandas")) # Save EvalStore results for later analysis
 
+def save_to_csv(csv_file: str):
+    results = museval.EvalStore() # Initialize EvalStore to hold separation quality results
+    results_path = os.path.join(results_dir, "separation_quality_results_eval.pandas") # Path to saved EvalStore results
+    results.load(results_path) # Load previously saved EvalStore results
+    try:
+        dataframe = results.agg_frames_scores() # Get aggregated frame-level scores (one metric per stem per track)
+        with open(csv_file, 'a', newline="") as cf:
+            writer = csv.writer(cf)
+            for track in dataframe.index.get_level_values("track").unique(): # Iterate over unique tracks
+                track_df = dataframe.loc[track] # DataFrame for the specific track
+                for stem in track_df.index.get_level_values("target").unique(): # Iterate over unique stems for the track
+                    stem_df = track_df.loc[stem] # DataFrame for the specific stem
+                    sdr = stem_df.loc["SDR"] # Median SDR for the stem
+                    sir = stem_df.loc["SIR"] # Median SIR for the stem
+                    sar = stem_df.loc["SAR"] # Median SAR for the stem
+                    isr = stem_df.loc["ISR"] # Median ISR for the stem
+                    writer.writerow([track, stem, sdr, sir, sar, isr]) # Write results to CSV
+
+                    print(f"{track} {stem}: "f"SDR={sdr:.3f}, SIR={sir:.3f}, SAR={sar:.3f}, ISR={isr:.3f}")
+        
+    except Exception as e:
+        print(f"failed to save overall results with error: {e}")
 
 if __name__ == "__main__":
-    test_separation_quality()
+    #test_separation_quality()
+    save_to_csv(csv_file)
